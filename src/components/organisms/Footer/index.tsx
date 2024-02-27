79efe19f5ee7c189c7d76a3315365e120d136f02
dennyzain <abbasdenny24@gmail.com>
2024-02-27 11:49:39 +0700

    chore: delete alll filesss haha

--- src/components/organisms/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="layout font-semibold font-inter text-xs text-center py-2">
-    <p>&copy; Denny Abbas Zain 2022</p>
-  </footer>
-);
-
-export default Footer;
--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { NextPage } from 'next';
-import Layout from '@/components/templates/Layout';
-
-const Home: NextPage = () => <Layout />;
-
-export default Home;
