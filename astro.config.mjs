f8e3fa77f49ee2d2920ba5df5574011b34853571
dennyzain <abbasdenny24@gmail.com>
2024-02-27 11:52:51 +0700

    feat: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind(),icon({iconDir: 'public/icons'})],
+});
