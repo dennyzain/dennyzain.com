2ce31725bc449b78ab09358fd61d8283fa95c22a
dennyzain <abbasdenny24@gmail.com>
2024-03-21 17:19:41 +0700

    fix: build

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Boy';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Astro Boy";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SOCIAL_LINKS = [
+  { name: "github", url: "https://www.github.com/dennyzain" },
+  { name: "linkedin", url: "https://www.linkedin.com/in/denny-abbas-zain/" },
+  { name: "medium", url: "https://www.medium.com/@abbasdenny" },
+  { name: "twitter", url: "https://www.twitter.com/abbas_denny" },
+];
