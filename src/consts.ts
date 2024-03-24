c31db1fa95edaa12f7a6552fedd5e3a619e42fe5
dennyzain <abbasdenny24@gmail.com>
2024-03-24 12:42:11 +0700

    feat: favicon and metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Boy";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Denny Abbas Zain";
+export const SITE_DESCRIPTION = "do it or never";
 export const SOCIAL_LINKS = [
   { name: "github", url: "https://www.github.com/dennyzain" },
   { name: "linkedin", url: "https://www.linkedin.com/in/denny-abbas-zain/" },
