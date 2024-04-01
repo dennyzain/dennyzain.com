f9847d1cf38766e9325134bee6bf912e5196a76d
dennyzain <abbasdenny24@gmail.com>
2024-04-01 21:09:43 +0700

    chore: setup shiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,25 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dennyabbaszain.me',
-  integrations: [mdx(), sitemap(), tailwind(),icon({iconDir: 'public/icons'})],
+  site: "https://dennyabbaszain.me",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    icon({ iconDir: "public/icons" }),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'one-dark-pro',
+      langs: [],
+      wrap: true,
+      transformers: [],
+    },
+  },
 });
