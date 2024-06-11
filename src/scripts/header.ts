c6ad395684da24b07530db0ecb93f3e9091e20b5
dennyzain <abbasdenny24@gmail.com>
2024-06-11 14:01:36 +0700

    feat: update experience

--- src/scripts/header.ts
+++ src/scripts/header.ts
@@ -27,7 +27,7 @@ interface PropsType {
 }
 
 const lenis = new Lenis({
-  duration: 2,
+  duration: 1.5,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
   gestureOrientation: "vertical",
   touchMultiplier: 2,
